774bb1f82cab6644dfc9a146f8502b9cc292da0e
Mr Kaehms <kaehmsrobert@dublinusd.org>
2017-10-19 00:56:11 -0700

    Initial sharing of project

---
+++ ArrayMistakes.java
@@ -0,0 +1,17 @@
+public class ArrayMistakes
+{
+    public static void main()
+    {
+      int [] nums;
+
+      System.out.print("\nForward");
+      for (int i = 1; i <= nums.length(); i++) {
+         System.out.print(": " + nums[i]);
+      }
+
+      System.out.print("\nBackward" );
+      for (int i = nums.length; i > 0; i--) {
+         System.out.print( ": " + nums[i]);
+      }
+}
+}
